8c213ddfbe69c6dc508df62c5c1be67cfdf0f747
Aaron Zhang <azhang413@gmail.com>
2023-10-14 18:42:33 -0400

    added scroll to resume

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@ if (document.getElementById('my-work-link')) {
     document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
   })
 }
+
+if (document.getElementById('my-resume-link')) {
+  document.getElementById('my-resume-link').addEventListener('click', () => {
+    document.getElementById('resume').scrollIntoView({behavior: "smooth"})
+  })
+}
