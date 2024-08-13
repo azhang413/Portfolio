947bd9fdf54f61a00660a983e1dadada26690d48
Aaron Zhang <72778815+azhang413@users.noreply.github.com>
2024-08-13 16:32:43 -0700

    new rotate

--- js/script.js
+++ js/script.js
@@ -16,3 +16,16 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+const wrapper = document.querySelector(".words");
+const CURRENT_CLASS = "current";
+setInterval(() => {
+  const currentWord = wrapper.querySelector("span.current");
+  const nextWord = currentWord.nextElementSibling
+    ? currentWord.nextElementSibling
+    : wrapper.firstElementChild;
+  currentWord.classList.remove(CURRENT_CLASS);
+  nextWord.classList.add(CURRENT_CLASS);
+  wrapper.style.setProperty("--color", nextWord.dataset.color);
+  wrapper.style.setProperty("--color-bg", nextWord.dataset.bgColor);
+}, 1500);
